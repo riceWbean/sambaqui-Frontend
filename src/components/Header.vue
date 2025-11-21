26c1b1fc533816b995bc3612fbe4e61cc3155fc5
vinyzada <vinyescolas@gmail.com>
2025-11-21 13:31:05 -0300

    feat: criado base do componente header #12

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <ul style="">
+            <li>Início</li>
+            <li>Exibições</li>
+            <li>Mapa</li>
+            <li>Acervo</li>
+            <li>Search</li>
+            <li>Icon</li>
+        </ul>
+    </header>
+</template>
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-
-
 <template>
+  <Header />
   <main>
     abacate
   </main>
 </template>
+
+<script setup>
+import Header from '@/components/Header.vue'
+</script>
