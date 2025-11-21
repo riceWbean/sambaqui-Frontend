3ff05bff06d91811d0645b01b417f53878638fce
dnzlucas <lucasdiniz1708@gmail.com>
2025-11-21 15:36:33 -0300

    FEAT: Create simple router #13

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/signin',
       name: 'signin',
       component: () => import('../views/signIn/SignInView.vue')
+    },
+    {
+      path: '/exhibitions',
+      name: 'exhibitions',
+      component: () => import('../views/exhibitions/ExhibitionsView.vue')
     }
   ],
 })
