b1c6dc02e7a27724a581e89eeeb7a4edd96ecbc1
vinyzada <vinyescolas@gmail.com>
2025-11-21 21:47:11 -0300

    feat: criada service e store para consumo da api de artefatos #34

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { MotionPlugin } from '@vueuse/motion'
 
 import App from './App.vue'
 import router from './router'
+import '@/plugins/api'
 
 import '@fortawesome/fontawesome-free/css/all.css';
 
