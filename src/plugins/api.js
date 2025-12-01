32f52dc67133f40e3017bf21b80bd58fe4bbb581
LuizBlume <luiz.fernando.251107@gmail.com>
2025-12-01 20:24:49 -0300

    fix: atualizar URL base da API para o ambiente de produção

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://sambaqui-backend.onrender.com/api',
   timeout: 10000,
   headers: {
     skipAuth: true
