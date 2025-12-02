3fc5b5f4403705c7c40eeacac8e8bb8320707e11
LuizBlume <luiz.fernando.251107@gmail.com>
2025-12-02 13:28:12 -0300

    fix: atualizar URL base da API para o ambiente de produção

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://sambaqui-backend.onrender.com/api',
+  baseURL: 'https://acervo-api.productionapp3.fabricadesoftware.ifc.edu.br/api',
   timeout: 10000,
   headers: {
     skipAuth: true
